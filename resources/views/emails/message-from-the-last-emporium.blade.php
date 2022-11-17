4f9b0b01405ed56e38f663cca25a5d2c243dbf13
Mark Yip <yip279@gmail.com>
2022-11-17 14:24:44 +0800

    Replace inline style with style tag + laravel-mail-css-inliner + css provided by rahul.

--- resources/views/emails/message-from-the-last-emporium.blade.php
+++ resources/views/emails/message-from-the-last-emporium.blade.php
@@ -1,21 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      max-width: 600px;
+      background-color: #0d4257;
+      color: #eadece;
+      font-family: monospace;
+      letter-spacing: 0.02em;
+      line-height: 1.5;
+      display: block;
+      margin: 40px auto;
+    }
+
+    .signoff {
+      width: 300px;
+      margin-top: 40px;
+      margin-left: auto;
+      margin-right: 0px;
+    }
+
+    #logo_image {
+      display: block;
+      margin: 40px auto;
+      width: 50%;
+    }
+
+    @media (max-width: 767px) {
+      body {
+        width: 100%;
+        padding: 0 20px;
+        box-sizing: border-box;
+      }
+
+      .signoff {
+        width: 60%;
+        margin-top: 40px;
+        margin-left: auto;
+        margin-right: 0px;
+      }
+    }
+  </style>
 </head>
-<body style="background-color: #0A242E; color: #E8DED0;">
-<div style="max-width: 1024px; 	padding-left: 8px; padding-right: 8px; margin-left:auto; margin-right: auto; padding-top: 40px;">
-  <p style="white-space: pre-wrap; font-family: monospace;">{{ $messageText }}</p>
-  <p style="text-align: right; white-space: pre-wrap; font-family: monospace;">{{ $from }}</p>
-  <p style="text-align: right; white-space: pre-wrap; font-family: monospace;">{{ $timestamp }}<br>{{ $location }}</p>
-  <a href="https://www.thelastemporium.hk" style="display: block; margin-top: 40px; margin-bottom: 40px;">
-    <img
-      src="{{ Storage::disk('public')->url('assets/the-logo.png') }}"
-      style="display: block; max-width:992px; margin-left:auto; margin-right: auto; width: 100%;"
-    >
+
+<body>
+  <p>{{ $messageText }}</p>
+  <p class="signoff">{{ $from }}</p>
+  <p>{{ $timestamp }}<br>{{ $location }}</p>
+  <a href="https://www.thelastemporium.hk">
+    <img id="logo_image" src="{{ Storage::disk('public')->url('assets/the-logo.png') }}">
   </a>
-</div>
 </body>
+
 </html>
